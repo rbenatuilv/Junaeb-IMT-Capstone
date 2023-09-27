27d071ef92e6bdea96d272339af06cad5895e1a4
ROBERTO BENATUIL <rbenatuilv@uc.cl>
2023-09-27 17:36:18 -0300

    add pickle creator of JUNAEB data

---
+++ Data/create_pickle_JUNAEB.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+import os
+
+
+folder = 'Consolidado2020'
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+ruta_pickle = 'DATA_JUNAEB.pkl'
+
+data = dict()
+for mes in meses:
+    path = os.path.join(folder, f'{mes}_JUNAEB.xlsx')
+    data[mes[:3]] = pd.read_excel(path)
+
+with open(ruta_pickle, 'wb') as archivo:
+    pickle.dump(data, archivo)
+
