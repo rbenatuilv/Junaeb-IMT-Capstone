b00081447eadcd6ec77801185234cb4737015f61
rbenatuilv <rbenatuilv@uc.cl>
2023-11-24 02:12:57 -0300

    feat: optimized closest schools finder with dijkstra

---
+++ modules/find_neighs.py
@@ -0,0 +1,104 @@
+import modules.graph as g
+import modules.geo as geo
+from shapely.geometry import Point
+import geopandas as gpd
+import heapq
+
+
+MAXD = geo.convert_to_degrees(200000)
+
+
+def get_closest(points, adj_list, node, k, mode='manhattan'):
+    """Returns the k closest nodes to the given node"""
+
+    visited = dict()
+    gen = set()
+    gen.add(node)
+
+    finished = False
+    last = False
+    
+    while gen and not finished:
+        if last:
+            finished = True
+
+        new_gen = set()
+
+        for next in gen:
+            visited[next] = g.distance(points[node], points[next], mode=mode)
+
+        for next in gen:
+            for neigh in adj_list[next]:
+                if neigh not in visited:
+                    d = g.distance(points[node], points[neigh], mode=mode)
+                    if d <= MAXD:
+                        new_gen.add(neigh)
+        
+        gen = new_gen
+
+        if len(visited) >= k:
+            last = True
+
+    visited.pop(node)
+    visited = list(visited.items())
+    visited.sort(key=lambda x: x[1])
+    visited = visited[:k]
+
+    tot_prob = sum([1 / x[1] for x in visited])
+    probs = [(1 / x[1]) / tot_prob for x in visited]
+    visited = [x[0] for x in visited]
+
+    return visited, probs
+
+
+def get_closest_djikstra(points, adj_list, node, k, mode='manhattan'):
+    """Returns the k closest nodes to the given node using Djikstra's algorithm"""
+
+    visited = dict()
+    heap = [(0, node)]
+
+    while heap:
+        dist, next = heapq.heappop(heap)
+        if next in visited:
+            continue
+        visited[next] = dist
+
+        for neigh in adj_list[next]:
+            if neigh not in visited:
+                d = g.distance(points[node], points[neigh], mode=mode)
+                if d <= MAXD:
+                    heapq.heappush(heap, (d, neigh))
+
+        if len(visited) > k:
+            break
+
+    visited.pop(node)
+    visited = list(visited.items())
+    visited.sort(key=lambda x: x[1])
+    visited = visited[:k]
+
+    tot_prob = sum([1 / x[1] for x in visited])
+    probs = [(1 / x[1]) / tot_prob for x in visited]
+    visited = [x[0] for x in visited]
+
+    return visited, probs
+
+
+def get_closest_intersect(gpd, node, k, mode='manhattan'):
+    p = (gpd['Longitud'][node], gpd['Latitud'][node])
+    point = Point(p)
+    buff = point.buffer(MAXD)
+    neighs = gpd[gpd.intersects(buff)]
+    neighs = neighs[neighs.index != node]
+
+    distancias = []
+    for i in neighs.index:
+        point = (neighs['Longitud'][i], neighs['Latitud'][i])
+        d = g.distance(p, point, mode=mode)
+        if len(distancias) < k:
+            heapq.heappush(distancias, (-d, i))
+        else:
+            heapq.heappushpop(distancias, (-d, i))
+
+    return [i for d, i in sorted(distancias, reverse=True)]
+
--- test.py
+++ test.py
@@ -4,16 +4,11 @@ import geopandas as gpd
 from tqdm import tqdm
 
 import modules.db_management as dbm
-import modules.geo as geo
+import modules.graph as g
 import modules.tsp as tsp
 
+from modules.find_neighs import get_closest, get_closest_intersect, get_closest_djikstra
 
-
-##################### Load data #########################
-
-# schools = pd.read_excel('(TEST) Colegios(Continental)2020(UTs).xlsx')
-
-# pp.single_plot_uts(7, schools, legend=True, markersize=10, marker='x', cmap='tab20')
 
 ##################### Efficiency test ###################
 
@@ -27,16 +22,39 @@ n = len(schools)
 
 tester = tsp.TSPApprox(schools, coords_labels=coords_labels, compile=False)
 
+r = range(100)
+
 list1 = []
-for node in tqdm(range(n)):
-    list1.append(tester.get_options(node))
+for node in tqdm(r):
+    list1.append((node, tester.get_options(node, mode='euclidean')[0]))
 
-print(list1)
 
-list2 = []
-for node in tqdm(range(n)):
-    list2.append(tester.get_options_optimized(node))
+# list2 = []
+x_label = coords_labels[0]
+y_label = coords_labels[1]
 
-print(list2)
+points = [(schools[x_label][i], schools[y_label][i]) for i in range(len(schools))]
+adj_list = g.get_adj_list(points)
 
-print(list1 == list2)
+# for node in tqdm(range(n)):
+#     list2.append(get_closest(points, adj_list, node, 25)[0])
+
+
+list3 = []
+for node in tqdm(r):
+    list3.append((node, get_closest_djikstra(points, adj_list, node, 25, mode='euclidean')[0]))
+
+
+# opt1 = list1[0]
+# # opt2 = list2[0]
+# opt3 = list3[0]
+
+# print(opt1)
+# # print(opt2)
+# print(opt3)
+
+for e1, e2 in zip(list1, list3):
+    if e1[1] != e2[1]:
+        print(e1)
+        print(e2)
+        print()
